e7c73c4b3bd0db42ed51606b0715f92b1a3e866b
MrJaydanOz <jaydan@jaysanlunt.com.au>
2024-03-20 16:33:29 +1100

    Reverted "PoseT"

--- Engine/Source/Private/Math/PoseT.cpp
+++ Engine/Source/Private/Math/PoseT.cpp
@@ -1,7 +1,7 @@
 #include "Math/PoseT.h"
 #include "Math/VectorT.h"
 
-template<typename TPos, typename TRot>
+/*template<typename TPos, typename TRot>
 Vector<2, TPos> Pose<TPos, TRot>::TransformPoint(const Vector<2, TPos>& point) const noexcept { return TransformDirection(point) + position; }
 
 template<typename TPos, typename TRot>
@@ -24,3 +24,21 @@ TRot Pose<TPos, TRot>::InverseTransformRotation(const TRot& rotation) const noex
 
 template<typename TPos, typename TRot>
 Pose<TPos, TRot> Pose<TPos, TRot>::InverseTransformPose(const Pose<TPos, TRot>& pose) const noexcept { return Pose(InverseTransformPoint(pose.position), InverseTransformRotation(pose.rotation)); }
+*/
+PoseF::PoseF(Vector2F position, float rotation) : position(position), rotation(rotation) { }
+
+Vector2F PoseF::TransformPoint(const Vector2F& point) { return TransformDirection(point) + position; }
+
+Vector2F PoseF::TransformDirection(const Vector2F& direction) { return direction.Rotate(rotation); }
+
+float PoseF::TransformRotation(const float& rotation) { return rotation + this->rotation; }
+
+PoseF PoseF::TransformPose(const PoseF& pose) { return PoseF(TransformPoint(pose.position), TransformRotation(pose.rotation)); }
+
+Vector2F PoseF::InverseTransformPoint(const Vector2F& point) { return InverseTransformDirection(point - position); }
+
+Vector2F PoseF::InverseTransformDirection(const Vector2F& direction) { return direction.InverseRotate(rotation); }
+
+float PoseF::InverseTransformRotation(const float& rotation) { return rotation - this->rotation; }
+
+PoseF PoseF::InverseTransformPose(const PoseF& pose) { return PoseF(InverseTransformPoint(pose.position), InverseTransformRotation(pose.rotation)); }
--- Engine/Source/Public/Math/PoseT.h
+++ Engine/Source/Public/Math/PoseT.h
@@ -1,4 +1,4 @@
-#pragma once
+/*#pragma once
 #include "VectorT.h"
 
 template<typename TPos, typename TRot>
@@ -21,4 +21,29 @@ public:
 };
 
 typedef Pose<float, float> PoseF;
-typedef Pose<double, double> PoseD;
+typedef Pose<double, double> PoseD;*/
+#pragma once
+#include "VectorT.h"
+
+#define DECLARATION_POSE(poseType, positionType, positionElementType, rotationType, ...) \
+struct poseType \
+{ \
+public: \
+    positionType position; \
+    rotationType rotation; \
+ \
+public: \
+	poseType(positionType position, rotationType rotation); \
+	positionType TransformPoint(const positionType& point); \
+	positionType TransformDirection(const positionType& direction); \
+	rotationType TransformRotation(const rotationType& rotation); \
+	poseType TransformPose(const poseType& pose); \
+	positionType InverseTransformPoint(const positionType& point); \
+	positionType InverseTransformDirection(const positionType& direction); \
+	rotationType InverseTransformRotation(const rotationType& rotation); \
+	poseType InverseTransformPose(const poseType& pose); \
+    __VA_ARGS__ \
+};
+
+DECLARATION_POSE(PoseF, Vector2F, float, float)
+DECLARATION_POSE(PoseD, Vector2D, double, double)
